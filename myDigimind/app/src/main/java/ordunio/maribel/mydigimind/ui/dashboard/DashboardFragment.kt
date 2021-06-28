f7d1247a0a9db55ed0c4702fbc9b83989371e0b7
Citlali Orduño <65271029+CitOrd@users.noreply.github.com>
2021-06-28 12:49:22 -0600

    digmind part 2- begining

--- myDigimind/app/src/main/java/ordunio/maribel/mydigimind/ui/dashboard/DashboardFragment.kt
+++ myDigimind/app/src/main/java/ordunio/maribel/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -1,15 +1,22 @@
 package ordunio.maribel.mydigimind.ui.dashboard
 
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import ordunio.maribel.mydigimind.R
 import ordunio.maribel.mydigimind.databinding.FragmentDashboardBinding
+import java.text.SimpleDateFormat
 
 class DashboardFragment : Fragment() {
 
@@ -20,6 +27,7 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,10 +39,21 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val btn_time: Button = root.findViewById(R.id.btn_tiempo)
+
+
+        btn_time.setOnClickListener {
+            val cal= Calendar.getInstance()
+            val timeSetListener= TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                cal.set(Calendar.MINUTE, minute)
+
+                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE), true).show()
+        }
+
         return root
     }
 
--- myDigimind/app/src/main/java/ordunio/maribel/mydigimind/ui/home/HomeFragment.kt
+++ myDigimind/app/src/main/java/ordunio/maribel/mydigimind/ui/home/HomeFragment.kt
@@ -1,21 +1,25 @@
 package ordunio.maribel.mydigimind.ui.home
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import ordunio.maribel.mydigimind.R
 import ordunio.maribel.mydigimind.databinding.FragmentHomeBinding
+import ordunio.maribel.mydigimind.ui.Task
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-
+    var tasks = ArrayList<Task>()
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -31,15 +35,58 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    fun filltask(){
+        tasks.add(Task("Practice 1", arrayListOf("Monday","Sunday"),"17:30"))
+        tasks.add(Task("Crying in my misery", arrayListOf("Tuesday"),"10:30"))
+        tasks.add(Task("Workout with Jazmin", arrayListOf("Monday","Tuesday","Wednesday","Friday"),"19:00"))
+        tasks.add(Task("Practice 4", arrayListOf("Wednesday"),"11:00"))
+        tasks.add(Task("Lecture club with Joel", arrayListOf("Saturday"),"10:00"))
+        tasks.add(Task("Go out with Bere", arrayListOf("Thursday"),"20:00"))
+        tasks.add(Task("Go to mall", arrayListOf("Sunday"),"9:30"))
+    }
+
+    private class AdapterTask: BaseAdapter {
+        var tasks = ArrayList<Task>()
+        var context : Context? = null
+
+        constructor(context: Context, tasks:ArrayList<Task>){
+            this.context= context
+            this.tasks= tasks
+        }
+
+        override fun getCount(): Int {
+            return tasks.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return tasks[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        @SuppressLint("ViewHolder")
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+          var task= tasks[position]
+            var inflator = LayoutInflater.from(context)
+            var vista = inflator.inflate(R.layout.task_view, null)
+
+            var tv_title = vista.findViewById(R.id.)
+        }
+
+
+    }
+
 }
